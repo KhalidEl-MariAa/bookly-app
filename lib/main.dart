2c07ff62e6bbbe47607f20a3ccd88b759d386988
KhalidEl-MariAa <khalidmaria13@gmail.comm>
2023-04-13 01:10:54 -0700

    import get pakage and create get material app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart' ;
 
 void main() {
-  runApp();
+  runApp(const Booklyapp());
 }
 
+class Booklyapp extends StatelessWidget {
+  const Booklyapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+    
+      
+          );
+      }
+}
