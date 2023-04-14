279df15d37814992180cf9383714ad66ae5d5d38
KhalidEl-MariAa <khalidmaria13@gmail.comm>
2023-04-14 00:21:49 -0700

    import flutter in splash page body

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class Booklyapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const GetMaterialApp(
+      theme: ThemeData.copyWith(scaffoldBackgroundColor: ),
     home: Splashpage(),
       
           );
