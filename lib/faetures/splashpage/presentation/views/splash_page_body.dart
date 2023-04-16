cd4f6cf8645a5d4a7fbec2034a4f6628685d1489
KhalidEl-MariAa <khalidmaria13@gmail.comm>
2023-04-16 01:55:18 -0700

    animation apply on text and CircleIndicator

--- lib/faetures/splashpage/presentation/views/splash_page_body.dart
+++ lib/faetures/splashpage/presentation/views/splash_page_body.dart
@@ -3,9 +3,24 @@ import 'dart:ui';
 import 'package:bookly_app/constants.dart';
 import 'package:flutter/material.dart';
 
-class SplashPageBody extends StatelessWidget {
+class SplashPageBody extends StatefulWidget {
   const SplashPageBody({super.key});
 
+  @override
+  State<SplashPageBody> createState() => _SplashPageBodyState();
+}
+
+class _SplashPageBodyState extends State<SplashPageBody> with TickerProviderStateMixin {
+  late AnimationController aanimationController;
+  late Animation<Offset> sliding;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    aanimationController=AnimationController(vsync: this,duration:const Duration(seconds: 5));
+    sliding=Tween<Offset>(begin: const Offset(0,5),end: Offset.zero).animate(aanimationController);
+    aanimationController.forward();
+  }
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -14,9 +29,20 @@ class SplashPageBody extends StatelessWidget {
       
       children: [Image.asset('assets/boook.png',width: 200,height: 200,),
       const SizedBox(height: 25,),
-     const Text('Bookly App',style: TextStyle(fontWeight:FontWeight.w900 ,fontSize: 23,),textAlign: TextAlign.center),
+     AnimatedBuilder(
+      animation: sliding,
+       builder: (context,_) {
+         return SlideTransition(position:sliding,child: const Text('Bookly App',style: TextStyle(fontWeight:FontWeight.w900 ,fontSize: 23,),textAlign: TextAlign.center));
+       }
+     ),
      const SizedBox(height: 50,),
-    const Center(child: CircularProgressIndicator(color: Colors.white,strokeWidth: 7,backgroundColor: Color.fromARGB(255, 240, 78, 66),))],
+    AnimatedBuilder(
+      animation: sliding,
+
+      builder: (context,_) {
+        return SlideTransition(position:sliding,child: const Center(child: CircularProgressIndicator(color: Colors.white,strokeWidth: 7,backgroundColor: Color.fromARGB(255, 240, 78, 66),)));
+      }
+    )],
     );
   }
 }
