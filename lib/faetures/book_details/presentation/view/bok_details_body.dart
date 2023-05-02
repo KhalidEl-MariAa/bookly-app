5fa61b8194ae584c3bda75f43ec2c392c344e469
KhalidEl-MariAa <khalidmaria13@gmail.comm>
2023-05-02 06:53:54 -0700

    add title and subtitle ,refactor rating view

--- lib/faetures/book_details/presentation/view/bok_details_body.dart
+++ lib/faetures/book_details/presentation/view/bok_details_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookly_app/faetures/book_details/presentation/view/widgets/bd_AppBar.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../../../homepage/presentation/view/widgets/listviewElement.dart';
 
@@ -16,7 +17,18 @@ class BookDetailsPageBody extends StatelessWidget {
             padding: EdgeInsets.all(13),
             child: BookDetailsAppBar(),
           ),
-          ListViewElement(height: MediaQuery.of(context).size.height*0.28,)],),
+          ListViewElement(height: MediaQuery.of(context).size.height*0.28,),
+           Padding(
+             padding: const EdgeInsets.only(top:32),
+             child: Text('The Book Title ',style: GoogleFonts.adamina().copyWith(fontSize:19,fontWeight: FontWeight.bold)),
+           ),
+           Padding(
+            padding: const EdgeInsets.symmetric(vertical:7.0,horizontal: 30),
+            child: Text('Book Description : What are the topics that the book discuss',
+            maxLines: 3,
+            overflow: TextOverflow.ellipsis
+            ,style: TextStyle(color: Colors.white.withOpacity(0.5),fontSize: 13)))],
+           ),
       ),
     );
   }
