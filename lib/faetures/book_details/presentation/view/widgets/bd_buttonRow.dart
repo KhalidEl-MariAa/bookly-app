fe9c68ff52e3791c692d1559220845032b3abb11
KhalidEl-MariAa <khalidmaria13@gmail.comm>
2023-05-03 07:53:22 -0700

    improve ui adaptation to screens

--- lib/faetures/book_details/presentation/view/widgets/bd_buttonRow.dart
+++ lib/faetures/book_details/presentation/view/widgets/bd_buttonRow.dart
@@ -14,11 +14,12 @@ class RowOfButtonActions extends StatelessWidget {
         ButtonAction(bkColor: Colors.white,
         text: '25.5 L.E',
         pressColor: Colors.amber,
-        widthh: 138,
+        widthh: MediaQuery.of(context).size.width*0.35,
         textcolor: Colors.black,
         borderRaduis: const BorderRadius.only(topLeft: Radius.circular(10),bottomLeft: Radius.circular(10)),),
          ButtonAction(bkColor: const Color.fromARGB(255, 255, 55, 55),
         text: 'Free Preview',
+        widthh: MediaQuery.of(context).size.width*0.35,
         textcolor: Colors.white,
         borderRaduis: const BorderRadius.only(topRight: Radius.circular(10),bottomRight: Radius.circular(10)),)
       ],
