e4be73351a1df73c53526f90f2d62318dd4495cd
KhalidEl-MariAa <khalidmaria13@gmail.comm>
2023-06-26 05:46:45 -0700

    create listview cubit implementation

--- lib/faetures/homepage/presentation/view_model/listviewBooks_cubit/cubit/list_view_books_cubit.dart
+++ lib/faetures/homepage/presentation/view_model/listviewBooks_cubit/cubit/list_view_books_cubit.dart
@@ -1,9 +1,25 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/faetures/homepage/data/models/book_model/book_model.dart';
+import 'package:bookly_app/faetures/homepage/data/repostries/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'list_view_books_state.dart';
 
 class ListViewBooksCubit extends Cubit<ListViewBooksState> {
-  ListViewBooksCubit() : super(ListViewBooksInitial());
+  ListViewBooksCubit(this.homeRepo) : super(ListViewBooksInitial());
+  final HomeRepo homeRepo;
+
+  Future<void> fetchListViewBooks()async{
+
+    emit(ListViewBooksLoading());
+    var result =await  homeRepo.fetchListviewBooks();
+   result.fold((failure) {
+    emit(ListViewBooksFailure(failure.errorMessage));
+    
+   }, 
+   (books) {
+    emit(ListViewBooksSuccess(books));
+   });
+
+  }
 }
