82fd604b884f81cbad420a691edc74588ded165a
KhalidEl-MariAa <khalidmaria13@gmail.comm>
2023-06-29 05:39:03 -0700

    use multiblocProvider to provide the cubits

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/core/reusable.dart';
 import 'package:bookly_app/core/routing.dart';
-import 'package:bloc/bloc.dart' ;
+import 'package:bookly_app/faetures/homepage/data/repostries/home_repo_impl.dart';
+import 'package:bookly_app/faetures/homepage/presentation/view_model/bestseller_cubit/cubit/best_seller_cubit.dart';
+import 'package:bookly_app/faetures/homepage/presentation/view_model/listviewBooks_cubit/cubit/list_view_books_cubit.dart';
+
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+
 
 void main() {
   runApp(const Booklyapp());
@@ -17,14 +24,12 @@ class Booklyapp extends StatelessWidget {
       MultiBlocProvider(
         providers: [
           BlocProvider(
-            create: (context) => SubjectBloc(),
+            create: (context) => BestSellerCubit(getit.get<HomeRepoImpl>()),
           ),
           BlocProvider(
-            create: (context) => SubjectBloc(),
+            create: (context) => ListViewBooksCubit(getit.get<HomeRepoImpl>()),
           ),
         ],
-        child: Container(),
-      )(
         child: MaterialApp.router(
         routerConfig: Routes.router,
         debugShowCheckedModeBanner: false,
